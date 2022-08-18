88807bad97974e2fd2831b943c1523c9bb4c6996
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-08-18 15:55:54 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide (){
+let divide = 2000/100
+return divide;
+}
+function square(){
+    let x = 2
+    let square = 2*2
+    return square;
+}
+function add (a,b) {
+const add = a+b
+return add;
+}
